10bf53fe75593cb272b8f014dcf7c7ab2d70478a
4amigostechnologies <4amigostechnologies@gmail.com>
2017-06-16 00:16:39 +0530

    changes to register.html and controller

--- DJMBeta3/www/js/registerCtrl.js
+++ DJMBeta3/www/js/registerCtrl.js
@@ -1,8 +1,31 @@
 angular.module('register', ['ngResource'])
-    .controller('registerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('registerCtrl', ['$scope','$http', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope,$http,$stateParams) {
+    $scope.user = {};
+    $scope.register = function () {
+
+        var config = {
+            headers : {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+
+        $http.post('', $scope.user, config)
+        .success(function (data, status, headers, config) {
+            $scope.PostDataResponse = data;
+        })
+        .error(function (data, status, header, config) {
+            $scope.ResponseDetails = "Data: " + data +
+                "<hr />status: " + status +
+                "<hr />headers: " + header +
+                "<hr />config: " + config;
+        });
+    
+        
+    }
+
 
 
 }]);
