fbec0db287769d345687c851ccf96d86926a08a5
4amigostechnologies <4amigostechnologies@gmail.com>
2017-07-13 22:55:30 +0530

    Changed the style of payment history to accordian list

--- DJMBeta3/www/js/historyCtrl.js
+++ DJMBeta3/www/js/historyCtrl.js
@@ -4,10 +4,19 @@ function ($scope,$http, $stateParams, $state, userHistoryInformation) {
      $scope.userHistoryInformation = userHistoryInformation;
     //$http.get("http://djmwebapi.djmjewels.com/PaymentHistory/1135").then(function (response) {
 
-    //    $scope.userHistoryInformation = response.data;
+    //    $scope.userHistoryInformation1 = response.data;
     //});
 
-
+    $scope.toggleGroup = function (group) {
+        if ($scope.isGroupShown(group)) {
+            $scope.shownGroup = null;
+        } else {
+            $scope.shownGroup = group;
+        }
+    };
+    $scope.isGroupShown = function (group) {
+        return $scope.shownGroup === group;
+    };
 
      console.log($scope.userHistoryInformation);
 }]);
